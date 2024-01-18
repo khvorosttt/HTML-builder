953f227308a10f1eb4161ac982aace917d0ad71f
khvorosttt <khvorosttt@yandex.by>
2024-01-19 01:13:20 +0300

    feat: add copying folders inside a folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-async function copyDir() {
-  const oldDirPath = path.join(__dirname, 'files');
-  const newDirPath = path.join(__dirname, 'files-copy');
+const oldDirPath = path.join(__dirname, 'files');
+const newDirPath = path.join(__dirname, 'files-copy');
+async function copyDir(oldDirPath, newDirPath) {
   console.log(newDirPath);
   await fs.promises.rm(newDirPath, { force: true, recursive: true });
   await fs.promises.mkdir(newDirPath, { recursive: true });
@@ -17,13 +17,20 @@ async function copyDir() {
         console.log(err.message);
       }
       for (let file of files) {
-        fs.promises.copyFile(
-          path.join(oldDirPath, file.name),
-          path.join(newDirPath, file.name),
-        );
+        if (file.isDirectory()) {
+          copyDir(
+            path.join(oldDirPath, file.name),
+            path.join(newDirPath, file.name),
+          );
+        } else {
+          fs.promises.copyFile(
+            path.join(oldDirPath, file.name),
+            path.join(newDirPath, file.name),
+          );
+        }
       }
     },
   );
 }
 
-copyDir();
+copyDir(oldDirPath, newDirPath);
